154a9fbaf515c37c6ea7a76325ffef114588efd6
Norman Bernard <nksb414@gmail.com>
2021-06-07 07:05:04 +0000

    pulled in required modules, database connection working

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+
+connection.connect(function(error){
+    if (error){
+        console.error(error.message);
+        return;
+    } else {
+        console.log('connected with id ' + connection.threadId);
+    }
+
+});
