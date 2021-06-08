1f47b5dc558a47e98143fbf39de4a7576fc21ca4
Norman Bernard <nksb414@gmail.com>
2021-06-08 02:40:31 +0000

    completed start employee manager function with initial inquirer questions

--- index.js
+++ index.js
@@ -16,6 +16,54 @@ connection.connect(function(error){
         return;
     } else {
         console.log('connected with id ' + connection.threadId);
+        startEmployeeManager();
     }
 
 });
+
+async function startEmployeeManager(){
+    const answers = await inquirer.prompt([
+        {
+            type: "list",
+            message: "Please choose from the following list to add, view or update departments, roles or employees.",
+            choices: [
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "View Departments",
+                "View Roles",
+                "View Employees",
+                "Update Roles",
+            ],
+            name: "choice",
+        }
+
+    ])
+
+    if (answers.choice === "Add Department"){
+//        addDepartment();
+            console.log(answers);
+    }   else if (answers.choice === "Add Role"){
+        //addRole();
+        console.log(answers);
+    }   else if (answers.choice === "Add Employee"){
+        //addEmployee();
+        console.log(answers);
+    }   else if (answers.choice === "View Departments"){
+        //viewDepartments();
+        console.log(answers);
+    }   else if (answers.choice === "View Roles"){
+        //viewRoles();
+        console.log(answers);
+    }   else if (answers.choice === "View Employees"){
+        //viewEmployees();
+        console.log(answers);
+    }   else if (answers.choice === "Update Roles"){
+        //updateRoles();
+        console.log(answers);
+    }   
+};
+
+
+
+
